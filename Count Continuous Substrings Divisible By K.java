ed473c0c1af2afc1e35978beeafddcfb096df882
Zehua Lin <linweihua0@gmail.com>
2016-09-07 16:25:57 -0700

    count continuous

---
+++ Count Continuous Substrings Divisible By K.java
@@ -0,0 +1,49 @@
+/*
+
+Count the number of continuous substrings divisible by k
+
+*/
+
+/*
+	Almost the same as "Count the number of substrings divisible by K" in which problem, substrings can be non-continuous.
+	
+	dp[i, r] = number of continuous substrings that ending at index i divisible by k
+	
+	dp[i, r] = (digit % k == r ? 1 : 0) + sum(dp[i - 1, j]) where j satisfy (j*10 + the ith number) % k == r   // only difference here
+	
+	(digit % k == r ? 1 : 0) means substrings that starting and ending at i
+	sum(dp[i - 1, j]) means substrings starting before i but ending at i - 1.
+	
+	Note that it would be much easier to update in this way:
+	for(digit in string) {   // from left to right
+		dp[i, digit % k] = 1 // substring starting at i
+		for(j in range[0-6]) {
+			dp[i, (10*j + digit) % k] += dp[i-1, j];
+		}
+	}
+	
+	
+	Boundary:
+	dp[-1, r] = 0
+	
+	Final answer is sum(dp[i,0]) for all i >= 0
+
+	Time O(k*n) Space O(k)
+
+*/
+
+public static int numContinuousSubstringDivisibleByK(String s, int k) {
+	int[] count = new int[k];
+	int total = 0;
+	for(char c : s.toCharArray()) {
+		int[] tmp = new int[k];
+		tmp[(c - '0') % k]++;   // starting at i
+		for(int i = 0; i < k; i++) {   // starting before and at i - 1
+			//tmp[i] = count[i];    difference between non-continuous and continuous
+			tmp[(10 * i + c - '0') % k] += count[i];
+		}
+		count = tmp;
+		total += count[0];  // add all satisfied substrings
+	}
+	return total;
+}
--- Count Substrings divisible by k.java
+++ Count Substrings divisible by k.java
@@ -8,12 +8,7 @@ Count the number of substrings divisible by k
 	dp[i, r] = number of substrings from first i chars of string whose remainder is r after divided by k
 	
 	dp[i, r] = dp[i - 1, r] + sum(dp[i - 1, j] where j satisfy (j*10 + the ith number) % k == r
-	Here we use a helper array to get the index j faster:
-	We add j into indexes[i] when (10*j) % k == i
-	So when we are searching for j such that (j*10 + digit) % k == i, 
-	which implies (j*10) % k = (i - digit) % k,
-	we can just use indexes[(i - digit) % k] to get those valid indexes.
-	
+
 	Boundary:
 	dp[0, 0] = 1 (empty string is divisible by k)
 	dp[0, j] = 0 for j > 0
@@ -21,34 +16,22 @@ Count the number of substrings divisible by k
 	Final result is dp[s.length(), 0] - 1
 	
 	Complexity analysis:
-	Each time update dp[i,r] only takes amortized O(1), since each possible remainder only exists once in the helper array.
-	So to update dp, we needs O(k*n) time. To create the helper array, it takes O(k^2） time. 
+	Each time update dp[i,r] only takes O(1)
+	So to update dp, we needs O(k*n) time.
 	
-	Time O(k*n + k^2), space O(k)
+	Time O(k*n), space O(k)
 	
 	When k is small, we can see it as time O(n), space O(1)
-	
-	Note that Java can return negative remainder, so we just use (a - b % k + k) % k to get positive remainder.
 */
 
 public static int numSubstringDivisibleByK(String s, int k) {
-	// Build helper array -- indexes
-	ArrayList<Integer>[] indexes = new ArrayList[k];
-	for(int i = 0; i < k; i++) {
-		indexes[i] = new ArrayList<>();
-		for(int j = 0; j < k ; j++) {
-			if((j * 10) % k == i) indexes[i].add(j);
-		}
-	}
-	// Update dp array -- count
 	int[] count = new int[k];
 	count[0] = 1;
 	for(char c : s.toCharArray()) {
 		int[] tmp = new int[k];
 		for(int i = 0; i < k; i++) {
 			tmp[i] = count[i];
-			for(Integer index : indexes[(i - (c - '0') % k + k) % k])
-				tmp[i] += count[index];
+			tmp[(10 * i + c - '0') % k] += count[i];
 		}
 		count = tmp;
 	}
